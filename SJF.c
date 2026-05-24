95cefb74a77e7b5dcd45d9b1d7596884c804599b
Sayanth P <vda24it056@cev.ac.in>
2026-05-24 23:21:03 +0530

    Update SJF.c

--- SJF.c
+++ SJF.c
@@ -1,57 +1,79 @@
 #include <stdio.h>
 
 int main() {
+
     int n, i, j;
-    int bt[10], wt[10], tat[10];
+    int bt[10], wt[10], tat[10], p[10];
     int temp;
     float tot_WT = 0, tot_TAT = 0;
 
-    // 1. START
-    // 2. Get number of processes
+    // Input number of processes
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    // 3. Get burst time of each process
+    // Input burst times
     printf("Enter burst time of each process:\n");
+
     for(i = 0; i < n; i++) {
-        printf("P%d: ", i+1);
+
+        p[i] = i + 1;
+
+        printf("P%d: ", p[i]);
         scanf("%d", &bt[i]);
     }
 
-    // 4. Sort processes based on burst time (SJF)
-    for(i = 0; i < n-1; i++) {
-        for(j = i+1; j < n; j++) {
+    // SJF Sorting
+    for(i = 0; i < n - 1; i++) {
+
+        for(j = i + 1; j < n; j++) {
+
             if(bt[i] > bt[j]) {
+
+                // Swap burst time
                 temp = bt[i];
                 bt[i] = bt[j];
                 bt[j] = temp;
+
+                // Swap process number
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
             }
         }
     }
 
-    // Waiting time for first process
+    // First waiting time
     wt[0] = 0;
 
-    // 5. Calculation of WT and TAT
+    // Calculate waiting time
     for(i = 1; i < n; i++) {
-        wt[i] = wt[i-1] + bt[i-1];
+
+        wt[i] = wt[i - 1] + bt[i - 1];
     }
 
+    // Calculate turnaround time
     for(i = 0; i < n; i++) {
+
         tat[i] = wt[i] + bt[i];
+
         tot_WT += wt[i];
         tot_TAT += tat[i];
     }
 
-    // 6. Display result
+    // Display output
     printf("\nProcess\tBT\tWT\tTAT\n");
+
     for(i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\n", i+1, bt[i], wt[i], tat[i]);
+
+        printf("P%d\t%d\t%d\t%d\n",
+               p[i], bt[i], wt[i], tat[i]);
     }
 
-    printf("\nAverage Waiting Time = %.2f", tot_WT / n);
-    printf("\nAverage Turnaround Time = %.2f\n", tot_TAT / n);
+    printf("\nAverage Waiting Time = %.2f",
+           tot_WT / n);
 
-    // 7. STOP
+    printf("\nAverage Turnaround Time = %.2f\n",
+           tot_TAT / n);
+
     return 0;
 }
