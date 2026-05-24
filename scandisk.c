9814e3f420bbb423ed3268728ce52e6f079d5006
Sayanth P <vda24it056@cev.ac.in>
2026-05-24 11:30:58 +0530

    Implement SCAN disk scheduling algorithm

---
+++ scandisk.c
@@ -0,0 +1,60 @@
+// SCAN Disk Scheduling without abs()
+
+#include <stdio.h>
+
+int main() {
+    int req[20], n, head, i, j, temp, diff, seek = 0;
+
+    printf("Enter number of requests: ");
+    scanf("%d", &n);
+
+    printf("Enter requests:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &req[i]);
+
+    printf("Enter initial head position: ");
+    scanf("%d", &head);
+
+    // Sorting
+    for(i = 0; i < n-1; i++) {
+        for(j = 0; j < n-i-1; j++) {
+            if(req[j] > req[j+1]) {
+                temp = req[j];
+                req[j] = req[j+1];
+                req[j+1] = temp;
+            }
+        }
+    }
+
+    printf("Seek Sequence: %d ", head);
+
+    // Move right
+    for(i = 0; i < n; i++) {
+        if(req[i] >= head) {
+
+            diff = req[i] - head;
+
+            seek = seek + diff;
+            head = req[i];
+
+            printf("-> %d ", head);
+        }
+    }
+
+    // Move left
+    for(i = n-1; i >= 0; i--) {
+        if(req[i] < head) {
+
+            diff = head - req[i];
+
+            seek = seek + diff;
+            head = req[i];
+
+            printf("-> %d ", head);
+        }
+    }
+
+    printf("\nTotal Seek Time = %d", seek);
+
+    return 0;
+}
