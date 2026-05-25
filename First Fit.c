bebca48b7e6e3cbcbb5cf6ad8e7af83b038fdf94
Sayanth P <vda24it056@cev.ac.in>
2026-05-25 19:56:41 +0530

    Refactor First Fit memory allocation program

--- First Fit.c
+++ First Fit.c
@@ -1,47 +1,53 @@
-#include<stdio.h>
-void main()
-{
-int bsize[10], psize[10], bno, pno, flags[10], allocation[10], i, j;
+// First Fit Memory Allocation
 
-for(i = 0; i < 10; i++)  
-{  
-    flags[i] = 0;  
-    allocation[i] = -1;  
-}  
-  
-printf("Enter no. of blocks: ");  
-scanf("%d", &bno);  
-printf("\nEnter size of each block: ");  
-for(i = 0; i < bno; i++)  
-    scanf("%d", &bsize[i]);  
-      
-printf("\nEnter no. of processes: ");  
-scanf("%d", &pno);  
-printf("\nEnter size of each process: ");  
-for(i = 0; i < pno; i++)  
-    scanf("%d", &psize[i]);  
+#include <stdio.h>
 
-for (int i = 0; i < pno; i++)  
-{  
-    for (int j = 0; j < bno; j++)  
-    {  
-        if (bsize[j] >= psize[i])  
-        {  
-            allocation[i] = j;  
-            bsize[j] -= psize[i];  
-            break;  
-        }  
-    }  
-}  
+int main() {
+    int bsize[10], psize[10], allocation[10];
+    int bno, pno, i, j;
 
-printf("\nProcess No.\tProcess Size\tBlock no.\n");  
-for (int i = 0; i < pno; i++)  
-{  
-    printf(" %d\t\t%d\t\t", i + 1, psize[i]);  
-    if (allocation[i] != -1)  
-        printf("%d\n", allocation[i] + 1);  
-    else  
-        printf("Not Allocated\n");  
-}
+    printf("Enter number of blocks: ");
+    scanf("%d", &bno);
 
+    printf("Enter block sizes:\n");
+    for(i = 0; i < bno; i++)
+        scanf("%d", &bsize[i]);
+
+    printf("Enter number of processes: ");
+    scanf("%d", &pno);
+
+    printf("Enter process sizes:\n");
+    for(i = 0; i < pno; i++)
+        scanf("%d", &psize[i]);
+
+    // Initialize allocation array
+    for(i = 0; i < pno; i++)
+        allocation[i] = -1;
+
+    // First Fit Allocation
+    for(i = 0; i < pno; i++) {
+        for(j = 0; j < bno; j++) {
+
+            if(bsize[j] >= psize[i]) {
+                allocation[i] = j;
+
+                bsize[j] = bsize[j] - psize[i];
+
+                break;
+            }
+        }
+    }
+
+    printf("\nProcess No\tProcess Size\tBlock No\n");
+
+    for(i = 0; i < pno; i++) {
+        printf("%d\t\t%d\t\t", i + 1, psize[i]);
+
+        if(allocation[i] != -1)
+            printf("%d\n", allocation[i] + 1);
+        else
+            printf("Not Allocated\n");
+    }
+
+    return 0;
 }
