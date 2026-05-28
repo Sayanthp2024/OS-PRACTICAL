fb4408e37f3d023024414315a06a096465a151b5
Sayanth P <vda24it056@cev.ac.in>
2026-05-28 20:11:10 +0530

    Update CSCAN.c

--- CSCAN.c
+++ CSCAN.c
@@ -6,6 +6,7 @@ void cscan(int arr[], int n, int head)
     int seek_count = 0;
     int distance, cur_track;
     int max = 199;
+    int head_count = 0;
 
     // Sort the requests
     for(i = 0; i < n; i++)
@@ -21,7 +22,7 @@ void cscan(int arr[], int n, int head)
         }
     }
 
-    int index;
+    int index = n;
 
     // Find the head position
     for(i = 0; i < n; i++)
@@ -33,35 +34,47 @@ void cscan(int arr[], int n, int head)
         }
     }
 
+    printf("\nSeek Sequence: %d ", head);
+
     // Move right
     for(i = index; i < n; i++)
     {
         cur_track = arr[i];
         distance = cur_track - head;
+
         seek_count += distance;
+        head_count++;
+
         head = cur_track;
 
-        printf("%d ", cur_track);
+        printf("-> %d ", cur_track);
     }
 
     // Move to end of disk
     seek_count += (max - head);
+
+    // Jump to beginning
+    seek_count += max;
     head = 0;
 
-    // Jump to beginning and continue
-    seek_count += max;
+    printf("-> %d ", head);
 
+    // Continue from beginning
     for(i = 0; i < index; i++)
     {
         cur_track = arr[i];
         distance = cur_track - head;
+
         seek_count += distance;
+        head_count++;
+
         head = cur_track;
 
-        printf("%d ", cur_track);
+        printf("-> %d ", cur_track);
     }
 
-    printf("\nTotal seek time = %d\n", seek_count);
+    printf("\n\nTotal Seek Time = %d", seek_count);
+    printf("\nHead Movement Count = %d\n", head_count);
 }
 
 int main()
@@ -72,16 +85,14 @@ int main()
     scanf("%d", &n);
 
     printf("Enter disk requests:\n");
+
     for(i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
     printf("Enter initial head position: ");
     scanf("%d", &head);
 
-    printf("Seek sequence:\n");
-
     cscan(arr, n, head);
 
     return 0;
 }
-        
